6cf30b9e8b0105ed93ab6d141b6ea9cd7f028d21
otumian-empire <popecan1000@gmail.com>
2023-08-04 09:48:48 +0000

    refactor main.go

--- main.go
+++ main.go
@@ -2,49 +2,86 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-// author: otumian popecan1000@gmail.com
+// author: otumian-empire popecan1000@gmail.com
 // project: simple calculator
 // description: description and other details can be found in the README.md
 // operation: get operands and operator (string) and return the results
 
-func main() {
-	fmt.Println("Simple Calculator")
+// put negative second operands in brackets: (-23)
+func blockOperand(operand int) string {
+	if operand < 0 {
+		return fmt.Sprintf("(%v)", operand)
+	}
 
-	// prompt user to enter operands
-	fmt.Print("Enter first operand: ")
-	var firstOperand int
-	fmt.Scan(&firstOperand)
+	return fmt.Sprintf("%v", operand)
+}
 
-	fmt.Printf("The first operand is %v\n\n", firstOperand)
+// read and return operand
+func getOperand(name string) int {
+	fmt.Printf("Enter %v operand: ", name)
+	var operand int
+	fmt.Scan(&operand)
 
-	// prompt user for second operand
-	fmt.Print("Enter second operand: ")
-	var secondOperand int
-	fmt.Scan(&secondOperand)
+	fmt.Printf("The %v operand is %v\n\n", name, blockOperand(operand))
 
-	fmt.Printf("The second operand is %v\n\n", secondOperand)
+	return operand
+}
 
-	// prompt user for second operand
-	fmt.Print("Enter second operand[+,-,*,/]: ")
+// read operator
+func getOperator() string {
+	fmt.Print("Enter operator [+,-,*,/]: ")
 	var operator string
 	fmt.Scan(&operator)
 
 	fmt.Printf("The operator is %v\n\n", operator)
 
+	return operator
+}
+
+func exitCode(message string) {
+	fmt.Println(message)
+	os.Exit(1)
+}
+
+func calculate(operator string, firstOperand, secondOperand int) int {
+	var result int
+
 	switch operator {
 	case "+":
-		fmt.Printf("%v %v %v = %v", firstOperand, operator, secondOperand, firstOperand+secondOperand)
+		result = firstOperand + secondOperand
 	case "-":
-		fmt.Printf("%v %v %v = %v", firstOperand, operator, secondOperand, firstOperand-secondOperand)
+		result = firstOperand - secondOperand
 	case "*":
-		fmt.Printf("%v %v %v = %v", firstOperand, operator, secondOperand, firstOperand*secondOperand)
+		result = firstOperand * secondOperand
 	case "/":
-		fmt.Printf("%v %v %v = %v", firstOperand, operator, secondOperand, firstOperand/secondOperand)
+		if secondOperand == 0 {
+			exitCode("Zero division for operator " + operator)
+		}
+
+		result = firstOperand / secondOperand
 	default:
-		fmt.Print("Operator not known")
+		exitCode("Operator not known")
 	}
 
+	return result
+}
+
+func displayResult(operator string, firstOperand, secondOperand, result int) {
+	fmt.Printf("%v %v %v = %v", blockOperand(firstOperand), operator, blockOperand(secondOperand), result)
 	fmt.Println("\n\nDone!!!")
+}
+
+func main() {
+	fmt.Println("Simple Calculator")
+
+	firstOperand := getOperand("first")
+	secondOperand := getOperand("second")
+	operator := getOperator()
+
+	result := calculate(operator, firstOperand, secondOperand)
+
+	displayResult(operator, firstOperand, secondOperand, result)
 }
