2084501bee4376f0fa80437f5f4974aecbf50ab9
otumian-empire <popecan1000@gmail.com>
2023-08-04 07:13:24 +0000

    implement simple calculator

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// author: otumian popecan1000@gmail.com
+// project: simple calculator
+// description: description and other details can be found in the README.md
+// operation: get operands and operator (string) and return the results
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Simple Calculator")
+
+	// prompt user to enter operands
+	fmt.Print("Enter first operand: ")
+	var firstOperand int
+	fmt.Scan(&firstOperand)
+
+	fmt.Printf("The first operand is %v\n\n", firstOperand)
+
+	// prompt user for second operand
+	fmt.Print("Enter second operand: ")
+	var secondOperand int
+	fmt.Scan(&secondOperand)
+
+	fmt.Printf("The second operand is %v\n\n", secondOperand)
+
+	// prompt user for second operand
+	fmt.Print("Enter second operand[+,-,*,/]: ")
+	var operator string
+	fmt.Scan(&operator)
+
+	fmt.Printf("The operator is %v\n\n", operator)
+
+	switch operator {
+	case "+":
+		fmt.Printf("%v %v %v = %v", firstOperand, operator, secondOperand, firstOperand+secondOperand)
+	case "-":
+		fmt.Printf("%v %v %v = %v", firstOperand, operator, secondOperand, firstOperand-secondOperand)
+	case "*":
+		fmt.Printf("%v %v %v = %v", firstOperand, operator, secondOperand, firstOperand*secondOperand)
+	case "/":
+		fmt.Printf("%v %v %v = %v", firstOperand, operator, secondOperand, firstOperand/secondOperand)
+	default:
+		fmt.Print("Operator not known")
+	}
+
+	fmt.Println("\n\nDone!!!")
 }
